68d3615c705ddc26949c35fae72d06cd062ddaf3
rafael.silva <rafaelsilva@seplag.mt.gov.br>
2023-09-25 09:34:32 -0400

    RegisterDTO Validation

--- src/main/java/br/edu/ifmt/cba/ifmthub/model/dto/RegisterDTO.java
+++ src/main/java/br/edu/ifmt/cba/ifmthub/model/dto/RegisterDTO.java
@@ -4,11 +4,27 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class RegisterDTO {
+	@Email
+	@NotNull
 	private String email;
+	@Size(min = 6)
+	@NotNull
 	private String password;
+	@NotBlank
 	private String fullName;
+	@Pattern(message = "Values allowed: ['M', 'F']", regexp = "\b(M|F)\b")
+	@NotNull
 	private char gender;
+	@Past
+	@NotNull
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate birthDate;
 	private String urlImgProfile;
